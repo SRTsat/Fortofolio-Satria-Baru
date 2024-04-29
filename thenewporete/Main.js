fd72c2a13a0a20b03a22e210684db5eb93cc7598
SatriaNJ <164471270+SRTsat@users.noreply.github.com>
2024-04-29 08:31:51 +0700

    Add files via upload

---
+++ thenewporete/Main.js
@@ -0,0 +1,53 @@
+/*======================== toggle icon navbar ===========================*/
+
+let nemuIcon = document.querySelector('#home');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
+/*======================== scroll ===========================*/
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach.apply(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /*======================== Sticky Nav ===========================*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*======================== remove toggle icon navbar ===========================*/
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active')
+};
+
+/*======================== Scrollww ===========================*/
+ScrollReveal({
+    distance: '80px',
+    duration: 2000.,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container,portofolio-box', { origin: 'buttom' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
+
+/*======================== Type JS ===========================*/
